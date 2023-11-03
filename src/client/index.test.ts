9ffa720b403497c7de0694f1d20d35bea4302695
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2023-11-03 10:14:48 +0100

    Add GET request client test

--- src/client/index.test.ts
+++ src/client/index.test.ts
@@ -1,7 +1,24 @@
+import { HttpClient } from '.';
+import { mockFetch } from '../tests/mock';
+
 describe('HTTP Client', () => {
-  it('should be able to do a GET request', () => {
-    // TODO
-    expect(true).toBe(true);
+  const http = new HttpClient('https://example.com');
+
+  it('should be able to do a GET request', async () => {
+    interface DataType {
+      foo: string;
+    }
+
+    mockFetch({
+      foo: 'bar',
+    });
+
+    // @ts-expect-error - ignore method being private
+    const data = await http.request<DataType>('/test', 'GET');
+
+    expect(data).toEqual({
+      foo: 'bar',
+    });
   });
 
   it('should be able to do a nullable GET request', () => {
