ba651228d9573621f147dc87b47689ef888e0d05
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2023-11-05 06:53:27 +0100

    Add tests template for http client

---
+++ src/client/index.test.ts
@@ -0,0 +1,41 @@
+describe('HTTP Client', () => {
+  it('should be able to do a GET request', () => {
+    // TODO
+    expect(true).toBe(true);
+  });
+
+  it('should be able to do a nullable GET request', () => {
+    // TODO
+    expect(true).toBe(true);
+  });
+
+  it('should be able to do a POST request', () => {
+    // TODO
+    expect(true).toBe(true);
+  });
+
+  it('should be able to do a PUT request', () => {
+    // TODO
+    expect(true).toBe(true);
+  });
+
+  it('should be able to do a PATCH request', () => {
+    // TODO
+    expect(true).toBe(true);
+  });
+
+  it('should be able to do a DELETE request', () => {
+    // TODO
+    expect(true).toBe(true);
+  });
+
+  it('should be able to handle a standardized error', () => {
+    // TODO
+    expect(true).toBe(true);
+  });
+
+  it('should be able to handle a non-standardized error', () => {
+    // TODO
+    expect(true).toBe(true);
+  });
+});
